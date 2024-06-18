15bf7468b89f9873aac6b1b9c14223ca7427c995
srushtisalekar <117978764+srushtisalekar@users.noreply.github.com>
2024-06-18 11:53:49 +0530

    updatess index.js

--- index.js
+++ index.js
@@ -4,30 +4,39 @@
 //PROMISES
 //GET https://icanhazdadjoke.com/
 
-const jokes = document.querySelector('#joke');  //1
-const jokeBtn = document.querySelector('#jokeBtn');
+ const jokes = document.querySelector('#joke');  //1
+            const jokeBtn = document.querySelector('#jokeBtn');
 
+                   /*first method:-*/
 
-// const generateJokes = () => {      //3
+            // const generateJokes = () => {
+            // fetch('https://icanhazdadjoke.com')
+            // .then((res) => {
+            //     console.log(res.json());
+            // }).catch((error) => {
+            //     console.log(error);
+            // })
 
-//     const setHeader = {      //4
-//         headers: {
-//             Accept : "application/json"
-//         }
-//     }
+            /*above code can not used buz this api forment is in html
+            form so that i will get the help the postman to convert to html to json*/
+                
+        //     const setHeader = {
+        //         headers: {
+        //             Accept : "application/json"
+        //         }
+        //     }
+        //     fetch('https://icanhazdadjoke.com', setHeader)
+        //     .then((res) => res.json() )
+        //     .then( (data) => {
+        //         jokes.innerHTML = data.joke;
+        //     }).catch((error) => {
+        //         console.log(error);
+        //     })
+        //   }
 
-//     fetch('https://icanhazdadjoke.com', setHeader) //3
-//     .then((res) => res.json() )
-//     .then( (data) => {
-//         jokes.innerHTML = data.joke;
-//     }).catch((error) => {
-//         console.log(error);
-//     })
-
-// }
-
-//simplify the  promises using the async and await method
-const generateJokes = async () => {
+//  second method is the improve it above code 
+          /*second methods:-*/
+          const generateJokes = async () => {
     try{
         const setHeader = {
             headers: {
